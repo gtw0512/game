f1a2bdce3a24c00f2ee2149de05da228b8122fa9
taewon <gtw0512>
2024-01-22 00:06:06 +0900

    animatedSpring

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,7 +7,7 @@ module.exports = {
     "eslint:recommended",
     "plugin:react/recommended",
     "plugin:@typescript-eslint/recommended",
-    "prettier"
+    "prettier",
   ],
   overrides: [],
   parser: "@typescript-eslint/parser",
